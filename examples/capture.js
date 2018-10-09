d58604950c976aae4cb8bc3c51dfd94ae6016900
pablo <pablo.gobe@gmail.com>
2018-10-09 18:05:32 +0100

    Added basic manifest

--- examples/capture.js
+++ examples/capture.js
@@ -4,7 +4,7 @@ let stream = vid.captureStream();
 let audioctx = new AudioContext();
 let mediaStreamNode = audioctx.createMediaStreamSource(stream);
 // create a script processor with input of size 4096, one input (the video) and one output (the audioctx.destination)
-let scriptProcessingNode = audioctx.createScriptProcessor(4096, 1, 1);
+let scriptProcessingNode = audioctx.createScriptProcessor(16384, 1, 1);
 scriptProcessingNode.onaudioprocess = function(audioProcessingEvent) {
   // some code here, getting the audio and sending it to the APIs
   console.log(audioProcessingEvent);
