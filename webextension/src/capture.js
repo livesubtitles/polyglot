0d9f1ce540a0b746fdb3ae81d24a47aaf49e61a6
George Ongetta-Dagnoni <gao16@ray12.doc.ic.ac.uk>
2018-10-10 16:04:18 +0100

    Modified injection code to work from caputre script. Modified capture script to check if video is playing before processing the audio event

--- webextension/src/capture.js
+++ webextension/src/capture.js
@@ -5,8 +5,13 @@ let mediaStreamNode = audioctx.createMediaStreamSource(stream);
 // create a script processor with input of size 16384, one input (the video) and one output (the audioctx.destination)
 let scriptProcessingNode = audioctx.createScriptProcessor(16384, 1, 1);
 scriptProcessingNode.onaudioprocess = function(audioProcessingEvent) {
-  // some code here, getting the audio and sending it to the APIs
-  console.log(audioProcessingEvent);
+  if (!vid.paused) {
+    //Send along pipeline
+    //Recieve translated text
+    //Display Text
+    //Handle grouping of chunks
+    console.log(audioProcessingEvent);
+  }
 };
 mediaStreamNode.connect(scriptProcessingNode);
 scriptProcessingNode.connect(audioctx.destination);
