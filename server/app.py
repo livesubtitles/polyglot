42f81c1b5badc48c75e406efd3613718f96d308c
George Ongetta-Dagnoni <george@dyn3168-191.wlan.ic.ac.uk>
2018-10-12 17:54:59 +0100

    Included flask_restful dependancy, written basic rest endpoint file

--- server/app.py
+++ server/app.py
@@ -1,6 +1,14 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_restful import Resource, Api
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
