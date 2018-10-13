5440336bf67d7a871420540b05e8868b9f86352c
namanwahi <namanwahi@yahoo.co.uk>
2018-10-13 22:01:19 +0100

    heroku setup

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask
-from flask_restful import Resource, Api
-
 app = Flask(__name__)
-api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+@app.route("/")
+def hello():
+    return "Polyglot - Live subtitling"
 
-api.add_resource(HelloWorld, '/')
+@app.route("/pablo")
+def pablo():
+    return "GOROSTIAGAAAAAAAAAAAAAAAAAAAAAAA"
 
-if __name__ == '__main__':
-    app.run(debug=True)
