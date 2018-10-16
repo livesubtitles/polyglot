30854dca17dea2195d1a176538d26c285fe182e9
GeorgiosKousouris <georgios.kousouris16@imperial.ac.uk>
2018-10-16 19:47:06 +0100

    [pg1816][gk1016]: Stream audio packets from front-end to python server

--- examples/websocket.js
+++ examples/websocket.js
@@ -5,3 +5,25 @@ sock.on('connection', function(res){
   console.log("Received: " + res);
   console.log(res);
 });
+
+let vid = document.getElementsByTagName("video")[0];
+let stream = vid.captureStream();
+let audioctx = new AudioContext();
+let mediaStreamNode = audioctx.createMediaStreamSource(stream);
+// create a script processor with input of size 16384, one input (the video) and one output (the audioctx.destination)
+let scriptProcessingNode = audioctx.createScriptProcessor(16384, 1, 1);
+scriptProcessingNode.onaudioprocess = function(audioProcessingEvent) {
+  if (!vid.paused) {
+    //Send along pipeline
+    //Recieve translated text
+    //Display Text
+    //Handle grouping of chunks
+    console.log(audioProcessingEvent);
+    sock.emit("audioprocess", {
+      channelData: audioProcessingEvent.inputBuffer.getChannelData(0),
+      sampleRate: audioProcessingEvent.inputBuffer.sampleRate,
+    });
+  }
+};
+mediaStreamNode.connect(scriptProcessingNode);
+scriptProcessingNode.connect(audioctx.destination);
