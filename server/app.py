8edb696e3d618c193173238c07283d6be64cfbf7
Eleni Alevra <ea1516@ic.ac.uk>
2018-10-17 13:42:44 +0100

    Implement sending data to REST backend and fetching result

--- server/app.py
+++ server/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
+from flask import request
+from speechtotext import *
+import json
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+CORS(app, resources={r"/subtitle": {"origins": "*"}})
 socketio = SocketIO(app)
 
 @app.route("/")
@@ -11,6 +16,11 @@ def hello():
 @app.route("/pablo")
 def pablo():
     return "GOROSTIAGAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+
+@app.route("/subtitle", methods=['POST'])
+def subtitle():
+    request_body = json.loads(request.data)
+    return "{\"subtitle\":\"" + get_subtitle(request_body['audio'], request_body['sampleRate']) + "\"}"
 
 @socketio.on('connect')
 def test_connect():
