f14299a5f24f1b93f9ad3e3ade8253abbb88e57a
namanwahi <namanwahi@yahoo.co.uk>
2018-10-27 19:09:56 +0100

    handles error response code

--- server/punctuate.py
+++ server/punctuate.py
@@ -1,6 +1,11 @@
 import io
 import requests
 
+HOST = 'http://bark.phon.ioc.ee/punctuator'
+LOWEST_STATUS_CODE = 200
+HIGHEST_STATUS_CODE = 399
 def punctuate(text):
-     r = requests.post('http://bark.phon.ioc.ee/punctuator', data = {'text':"hello world"})
-     return r.text
+     r = requests.post(HOST, data = {'text':"hello world"})
+     if r.status_code >= 200 and r.status_code <= 399:
+          return r.text
+     return text
