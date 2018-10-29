40c97503d5fa9acf823eab00a8eefd8adf4798df
George Ongetta-Dagnoni <george@Georges-MacBook-Pro.local>
2018-10-29 21:00:18 +0000

    Pushed stream.py file. Needs much work but pulls audio from twitch and calls speech to text

---
+++ stream.py
@@ -0,0 +1,41 @@
+import io
+import subprocess
+import streamlink
+import server.speechtotext as stt
+import server.translate as trn
+from ffmpy import FFmpeg
+
+def create_wav_from_stream(stream_url):
+
+	available_streams = streamlink.streams(stream_url)
+	try:
+		audio_stream = available_streams['audio_only']
+	except KeyError:
+		#Handle video using available_streams['worst']
+		exit()
+
+	stream_data = audio_stream.open()
+
+	with open("audio.ts", "ab") as f:
+		for i in range(1, 5):
+			f.write(stream_data.read(100000))
+
+	audio_stream.close()
+
+	FFmpeg(
+    	inputs={'audio.ts': ['-ac', '1']},
+    	outputs={'audio.wav': ['-ac', '1']}
+	).run()
+
+	with open("audio.wav", "rb") as f:
+		content = f.read()
+
+	wav_mem_file = io.BytesIO(content)
+
+	return wav_mem_file
+
+	output = subprocess.check_output('ffprobe -v error -show_entries stream=sample_rate -of default=noprint_wrappers=1 audio.wav', shell=True)
+	sample_rate = output.decode('utf-8').split('=')[1]
+
+	text = stt.speech_to_text(wav_mem_file, sample_rate, stt.detect_language(wav_mem_file))
+
