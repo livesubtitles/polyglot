74f586e163f129c98812f6c5474f8098b72d794c
George Ongetta-Dagnoni <george@dyn3170-164.wlan.ic.ac.uk>
2018-10-30 20:29:39 +0000

    Tidied streamer with stop method

--- server/stream.py
+++ server/stream.py
@@ -1,30 +1,107 @@
-from streamlink import Streamlink
-import server.speechtotext
+import io
+import os
+import subprocess
+import streamlink
+import wave
+import sys
+import time
+import server.speechtotext as stt
+import server.translate as trn
 from ffmpy import FFmpeg
+from six.moves import queue
+from threading import Thread
 
-session = Streamlink()
-streams = session.streams("https://www.twitch.tv/pazos64")
+BYTES_TO_READ = 100000
 
-fd = streams['audio_only'].open()
-f = open("audio.ts", "ab")
+class _StreamWorker(Thread):
+	def __init__(self, buff, stream_data):
+		self.buff = buff
+		self.stream_data = stream_data
+		self.streaming = True
+		Thread.__init__(self)
 
-for i in range(1, 10):
-    data1 = fd.read(float('inf'))
-    f.write(data1)
-    print("Written to file")
+	def run(self):
+		while self.streaming:
+			data = self.stream_data.read(BYTES_TO_READ)
+			if data != '':
+				print("Getting data...")
+				self.buff.put(data)
 
-f.close()
+		self.stream_data.close()
 
-ff = FFmpeg(
-    inputs={'audio.ts': ['-acodec', 'copy']},
-    outputs={'audio.wav': ['-vodec', 'copy']}
-)
+	def stop_queue_worker(self):
+		self.streaming = False
 
-print(ff.cmd)
+class Streamer(object):
 
-##with open("audio.wav", "r") as f:
-##	content = f.read()
+	def __init__(self, stream_url):
+		self.stream_url = stream_url
+		self.buff = queue.Queue()
+        self.worker = None
+		self.sample_rate = None
 
-#print(speechtotext.speech_to_text(content, 48000, "es"))
+	def get_sample_rate(self):
+		"""
+			Returns the sample rate. This is not available until the
+			first time get_data is called (we return None).
+		"""
+		return self.sample_rate
 
-fd.close()
+	def get_data(self, num_segments):
+
+		with open("audio.ts", "ab") as f:
+			for x in range(num_segments):
+				data = self.buff.get()
+				f.write(data)
+
+		FFmpeg(
+    		inputs={'audio.ts': ['-ac', '1']},
+    		outputs={'audio.wav': ['-ac', '1']}
+		).run()
+
+		with open("audio.wav", "rb") as f:
+			content = f.read()
+
+		if not self.sample_rate:
+			wav = wave.open("audio.wav", "rb")
+			self.sample_rate = wav.getframerate()
+			wav.close()
+
+		os.remove("audio.ts")
+		os.remove("audio.wav")
+
+		return io.BytesIO(content)
+
+	def stream(self, stream_data):
+		print("Getting data...")
+		while streaming:
+			data = stream_data.read(BYTES_TO_READ)
+			if data != '':
+				self.buff.put(data)
+
+		stream_data.close()
+
+	def get_audio(self, available_streams):
+		try:
+			audio_stream = available_streams['audio_only']
+		except KeyError:
+			#Handle video using available_streams['worst']
+			exit()
+
+		return audio_stream
+
+	def start(self):
+		try:
+			available_streams = streamlink.streams(self.stream_url)
+		except Exception:
+			raise
+
+		audio_stream = self.get_audio(available_streams)
+
+		stream_data = audio_stream.open()
+
+		self.worker = _StreamWorker(self.buff, stream_data)
+		self.worker.start()
+
+	def stop(self):
+		self.worker.stop_queue_worker()
