69f02af91bddad5d61aff5794b13500adeae55d6
George Ongetta-Dagnoni <george@dyn3170-164.wlan.ic.ac.uk>
2018-10-30 19:22:46 +0000

    Created streaming object that continuously reads from the livestream, and will process data on request

--- stream.py
+++ stream.py
@@ -1,40 +1,86 @@
 import io
+import os
 import subprocess
 import streamlink
+import wave
 import server.speechtotext as stt
 import server.translate as trn
 from ffmpy import FFmpeg
+from six.moves import queue
+from threading import Thread
 
-def create_wav_from_stream(stream_url):
+class Streamer(object):
 
-	available_streams = streamlink.streams(stream_url)
-	try:
-		audio_stream = available_streams['audio_only']
-	except KeyError:
-		#Handle video using available_streams['worst']
-		exit()
+	BYTES_TO_READ = 100000
 
-	stream_data = audio_stream.open()
+	def __init__(self, stream_url):
+		self.stream_url = stream_url
+		self.buff = queue.Queue()
+		self.streaming = False
+		self.sample_rate = None
 
-	with open("audio.ts", "ab") as f:
-		for i in range(1, 5):
-			f.write(stream_data.read(100000))
 
-	stream_data.close()
+	def get_sample_rate(self):
+		"""
+			Returns the sample rate. This is not available until the
+			first time get_data is called (we return None).
+		"""
+		return self.sample_rate
 
-	FFmpeg(
-    	inputs={'audio.ts': ['-ac', '1']},
-    	outputs={'audio.wav': ['-ac', '1']}
-	).run()
+	def get_data(self, num_segments):
 
-	with open("audio.wav", "rb") as f:
-		content = f.read()
+		with open("audio.ts", "ab") as f:
+			for x in range(num_segments):
+				data = self.buff.get()
+				f.write(data)
 
-	wav_mem_file = io.BytesIO(content)
+		FFmpeg(
+    		inputs={'audio.ts': ['-ac', '1']},
+    		outputs={'audio.wav': ['-ac', '1']}
+		).run()
 
-	return wav_mem_file
+		with open("audio.wav", "rb") as f:
+			content = f.read()
 
-	output = subprocess.check_output('ffprobe -v error -show_entries stream=sample_rate -of default=noprint_wrappers=1 audio.wav', shell=True)
-	sample_rate = output.decode('utf-8').split('=')[1]
+		if not self.sample_rate:
+			wav = wave.open("audio.wav", "rb")
+			self.sample_rate = wav.getframerate()
+			wav.close()
 
-	text = stt.speech_to_text(wav_mem_file, sample_rate, stt.detect_language(wav_mem_file))
+		os.remove("audio.ts")
+		os.remove("audio.wav")
+
+		return io.BytesIO(content)
+
+	def stream(self, stream_data):
+		while streaming:
+			data = stream_data.read(BYTES_TO_READ)
+			if data !== '':
+				self.buff.put(data)
+
+		stream_data.close()
+
+	def get_audio(self, available_streams):
+		try:
+			audio_stream = available_streams['audio_only']
+		except KeyError:
+			#Handle video using available_streams['worst']
+			exit()
+
+	def start(self):
+		try:
+			available_streams = streamlink.streams(self.stream_url)
+		except Exception:
+			raise
+
+		audio_stream = self.get_audio(available_streams)
+
+		stream_data = audio_stream.open()
+
+		t = Thread(target=self.stream, args=(stream_data))
+
+		self.streaming = True
+		t.start()
+
+	def stop(self):
+		self.streaming = False
