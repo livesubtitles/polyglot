3bbcffcda3b2e490376b2927d0c9ab76b076c7dd
Eleni Alevra <ea1516@ic.ac.uk>
2018-10-31 18:10:45 +0000

    Limit detect language loop

--- server/speechtotext.py
+++ server/speechtotext.py
@@ -110,7 +110,11 @@ def detect_language(audio_file):
         video_id = (r.json())['id']
         print ("The videoId is " + video_id)
         source_lang = None
+        times = 0
         while(source_lang == None or source_lang == 'en-US'):
+            times = times + 1
+            if (times > 10):
+                return "en-US"
             print("HERE")
             time.sleep(2)
             print("HERE TOO")
