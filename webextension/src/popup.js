5f83d87247703a6f643aad90ff0a98e663edca30
GeorgiosKousouris <georgios.kousouris16@imperial.ac.uk>
2018-10-31 14:24:42 +0000

    All but capture.js merged

--- webextension/src/popup.js
+++ webextension/src/popup.js
@@ -1,3 +1,11 @@
+let languageSelector = document.getElementById('languageSelector');
+ languageSelector.onchange = () => {
+  updateLanguage(languageSelector);
+}
+function updateLanguage(selectLanguage) {
+  localStorage.setItem("selectedLanguage", selectLanguage.value);
+}
+
 let translateButton = document.getElementById('translateButton');
  translateButton.onclick = function(elem) {
    chrome.tabs.query({active: true, currentWindow: true},
