8c5be3a01981938eeb189d7d6f98043c5a07247b
eleni <elenialevra98@gmail.com>
2018-11-08 16:45:29 +0000

    Fix condition for generating punctuator model file in punctuate.py

--- server/punctuate.py
+++ server/punctuate.py
@@ -71,7 +71,7 @@ def remove_punctuation_annotations(subtitle):
 
 
 def punctuate_subtitle(subtitle):
-    if (os.path.isfile("./punctuator2/final-model.pcl")):
+    if (not os.path.isfile("./punctuator2/final-model.pcl")):
         prepare_model_file()
     preprocessed_text = preprocess_text(subtitle)
     #model_path = "./punctuator2/Demo-Europarl-EN.pcl"
