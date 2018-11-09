b7ffc5bae37a77bec686c92fa3c49d7b9c604cff
Hang Li Li <hang1997li@gmail.com>
2018-11-09 14:52:10 +0000

    Implemented the test cases for SpeechToText and languageDetecting.

--- server/language.py
+++ server/language.py
@@ -12,68 +12,74 @@ _LIMIT = 10
 _DEFAULT = "en-US"
 
 def _get_access_token(headers):
-	url = 'https://api.videoindexer.ai/auth/trial/Accounts/' + microsoftId + '/AccessToken?allowEdit=true'
-	response = requests.get(url, headers=headers)
-	return response.text.split("\"")[1]
+    url = 'https://api.videoindexer.ai/auth/trial/Accounts/' + microsoftId + '/AccessToken?allowEdit=true'
+    response = requests.get(url, headers=headers)
+    return response.text.split("\"")[1]
 
 def _get_id_url(access_token):
-	url = 'https://api.videoindexer.ai/trial/Accounts/' + microsoftId
-	url = url + '/Videos?accessToken=' + access_token
-	url = url + '&name=test' + str(random.randint(1, 100))
+    url = 'https://api.videoindexer.ai/trial/Accounts/' + microsoftId
+    url = url + '/Videos?accessToken=' + access_token
+    params = urllib.parse.urlencode({'language': 'auto'})
+    url = url + '?' + params
+    # url = url + '&name=test' + str(random.randint(1, 100))
 
-	return url
+    return url
 
 def _get_video_id(audio_file, headers, access_token):
-	url = _get_id_url(access_token)
+    url = _get_id_url(access_token)
 
-	form_data = {'file': audio_file.getvalue()}
-	params = urllib.parse.urlencode({'language': 'auto'})
+    form_data = {'file': audio_file.getvalue()}
 
-	r = requests.post(url, params=params, files=form_data, headers=headers)
-	return (r.json())['id']
+    r = requests.post(url, files=form_data, headers=headers)
+    return (r.json())['id']
 
 def _detection_request(video_id, headers, access_token):
-	url = 'https://api.videoindexer.ai/trial/Accounts/' + microsoftId 
-	url = url + '/Videos/' + video_id
-	url = url + '/Index?accessToken=' + access_token
+    url = 'https://api.videoindexer.ai/trial/Accounts/' + microsoftId
+    url = url + '/Videos/' + video_id
+    url = url + '/Index?accessToken=' + access_token
 
-	r = requests.get(url, headers=headers)
-	return (r.json())['videos'][0]['insights']['sourceLanguage']
+    r = requests.get(url, headers=headers)
+    return (r.json())['videos'][0]['insights']['sourceLanguage']
 
 
 ########### PUBLIC FUNCTIONS ###########
 
 def detect_language(audio_file):
-	print("Detecting Language...")
-	headers = {'Ocp-Apim-Subscription-Key': microsoftKey}
-	access_token = _get_access_token(headers)
-	
-	try:
-		video_id = _get_video_id(audio_file, headers, access_token)
-	except Exception as e:
-		print("Error with finding video ID from Microsoft API")
-		print(str(e))
-		return "en-US"
+    print("Detecting Language...")
+    headers = {'Ocp-Apim-Subscription-Key': microsoftKey}
+    access_token = _get_access_token(headers)
 
-	source_lang = None
-	try_no = 0
+    try:
+        video_id = _get_video_id(audio_file, headers, access_token)
+    except Exception as e:
+        print("Error with finding video ID from Microsoft API")
+        print(str(e))
+        return "en-US"
 
-	while(source_lang == None or source_lang == _DEFAULT): 
-		if (try_no > _LIMIT):
-			print("\tDetection limit reached. Defaulting to " + _DEFAULT)
-			return _DEFAULT
+    source_lang = None
+    try_no = 0
 
-		time.sleep(2)
+    try:
+        while(source_lang == None or source_lang == _DEFAULT):
+            if (try_no > _LIMIT):
+                print("\tDetection limit reached. Defaulting to " + _DEFAULT)
+                return _DEFAULT
 
-		print("\tFound: ", end='')
-		source_lang = _detection_request(video_id, headers, access_token)
-		
-		print (source_lang, end='')
-		if source_lang == None:
-			print(" (Retrying)")
+            time.sleep(2)
 
-		try_no = try_no + 1
+            print("\tFound: ", end='')
+            source_lang = _detection_request(video_id, headers, access_token)
+
+            print (source_lang, end='')
+            if source_lang == None:
+                print(" (Retrying)")
+
+            try_no = try_no + 1
+    except Exception as e:
+        print("Error with retrieving source language of the video")
+        print(str(e))
+        return "en-US"
 
 
-	print("\n\n\n")
-	return source_lang
+    print("\n\n\n")
+    return source_lang
