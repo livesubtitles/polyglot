655b84e3a7bf5869df29cdc28eb99366bc070021
eleni <elenialevra98@gmail.com>
2018-11-10 15:45:46 +0000

    Add html for embed button

---
+++ webextension/src/embed.js
@@ -0,0 +1,5 @@
+embedButton.onclick = function(elem) {
+       document.getElementById('embedButton').style.visibility = 'hidden';
+       document.getElementById('embedButton').style.display = 'none';
+       document.getElementById('embedText').style.visibility = 'visible';
+};
