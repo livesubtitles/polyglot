373dd7c19bc564cb1af4e4123fb157c390f7b30b
g-ongetta <georgeongetta@gmail.com>
2018-11-10 18:38:05 +0000

    Added new streaming architecture that stores files and adds them to the playlist

--- server/app.py
+++ server/app.py
@@ -1,9 +1,14 @@
 import json
 import jsonpickle
+import string
+import random
+import os
+import shutil
+
 
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, Namespace
 
 from server.translate import test
 from server.speechtotext import *
@@ -12,98 +17,160 @@ from server.stream import *
 
 app = Flask(__name__)
 CORS(app, resources={r"/subtitle": {"origins": "*"}, "/stream": {"origins": "*"}, "/stream-subtitle": {"origins": "*"}
-, "/set-language": {"origins": "*"}, "/get-language": {"origins": "*"}, "/punctuate": {"origins": "*"}})
+	, "/set-language": {"origins": "*"}, "/get-language": {"origins": "*"}, "/punctuate": {"origins": "*"}})
 socketio = SocketIO(app)
 streamer = None
 language = ""
 
+LOCAL_URL  = 'http://localhost:8000/'
+SERVER_URL = 'https://ployglot-livesubtitles.herokuapp.com/'
+
 # Main pipeline. Will return the JSON response with the translated text.
 def process(audio, sample_rate, lang, raw_pcm=False):
-    if lang == '':
-        lang = detect_language(audio)
+	if lang == '':
+		lang = detect_language(audio)
 
-    transcript = get_text_from_pcm(audio, sample_rate, lang) if raw_pcm else \
-                 get_text(audio, sample_rate, lang)
+		transcript = get_text_from_pcm(audio, sample_rate, lang) if raw_pcm else \
+		get_text(audio, sample_rate, lang)
 
-    translated = translate(transcript, 'en', lang.split('-')[0])
-    return jsonify(subtitle=translated, lang=lang)
+		translated = translate(transcript, 'en', lang.split('-')[0])
+		return jsonify(subtitle=translated, lang=lang)
 
 def process_with_video(video, audio, sample_rate, lang):
-    if lang == '':
-        #TODO: Move the split into the detect_language function
-        lang = detect_language(audio)
+	if lang == '':
+	#TODO: Move the split into the detect_language function
+		lang = detect_language(audio)
 
-    transcript = get_text(audio, sample_rate, lang)
-    translated = translate(transcript, 'en', lang.split('-')[0])
+	transcript = get_text(audio, sample_rate, lang)
+	translated = translate(transcript, 'en', lang.split('-')[0])
 
-    return jsonify(video=jsonpickle.encode(video), subtitle=translated, lang=lang)
+	return jsonify(video=jsonpickle.encode(video), subtitle=translated, lang=lang)
 
 def _initialise_streamer(url):
-    global streamer
+	global streamer
 
-    streamer = VideoStreamer(url)
+	streamer = VideoStreamer(url)
 
-    try:
-        streamer.start()
-    except Exception:
-        return _error_response( "StreamlinkUnavailable ")
+	try:
+		streamer.start()
+	except Exception:
+		return _error_response( "StreamlinkUnavailable ")
 
 def _error_response(error):
-    return jsonify(subtitle="", lang="", error=error)
+	return jsonify(subtitle="", lang="", error=error)
 
-################# REST ENDPOINTS #################
+ ################# REST ENDPOINTS #################
 
 @app.route("/")
 def hello():
-    return "Polyglot - Live Subtitles - ICL"
+	return "Polyglot - Live Subtitles - ICL"
 
 @app.route("/subtitle", methods=['POST'])
 def subtitle():
-    request_body = json.loads(request.data)
+	request_body = json.loads(request.data)
 
-    audio = request_body['audio']
-    sample_rate = request_body['sampleRate']
-    lang = request_body['lang']
+	audio = request_body['audio']
+	sample_rate = request_body['sampleRate']
+	lang = request_body['lang']
 
-    return process(audio, sample_rate, lang, raw_pcm=True)
+	return process(audio, sample_rate, lang, raw_pcm=True)
 
 @app.route("/set-language", methods=['POST'])
 def select_language():
-    global language
-    language = str(request.data).split('\'')[1]
-    return "Success"
+	global language
+	language = str(request.data).split('\'')[1]
+	return "Success"
 
 @app.route("/get-language", methods=['GET'])
 def get_language():
-    global language
-    return language
+	global language
+	return language
 
 @app.route("/stream", methods=['POST'])
 def stream():
-    global streamer
+	global streamer
 
-    if streamer == None:
-        _initialise_streamer(json.loads(request.data)['url'])
+	if streamer == None:
+		_initialise_streamer(json.loads(request.data)['url'])
 
-    lang = json.loads(request.data)['lang']
-    (video, audio) = streamer.get_data()
-    sample_rate    = streamer.get_sample_rate()
+	lang = json.loads(request.data)['lang']
+	(video, audio) = streamer.get_data()
+	sample_rate    = streamer.get_sample_rate()
 
-    return process_with_video(video, audio, sample_rate, lang)
+	return process_with_video(video, audio, sample_rate, lang)
 
 @app.route("/translate-test")
 def dummyTranslate():
-    return test()
+	return test()
 
-@app.route("/streams/<path:filename>")
-def getFile(filename):
-    return send_from_directory('streams', filename)
+@app.route("/streams/<path:user_dir>/<path:filename>")
+def getFile(user_dir, filename):
+	return send_from_directory('streams/' + user_dir, filename)
 
 ################# SOCKETS #################
 
-@socketio.on('connect')
-def test_connect():
-    print("Connected.")
+class StreamingSocket(Namespace):
+
+	_HASH_LEN = 20
+
+	streamer  = None
+	language  = None
+	user_hash = None
+	user_dir  = None
+
+	def _initialise_streamer(self, url):
+		self.streamer = VideoStreamer(url, self.user_dir)
+
+		try:
+			self.streamer.start()
+		except Exception:
+			emit('stream-response', json.dumps({'media':'', 'error':'Streamlink Unavailable!'}))
+
+	def _generate_user_hash(self):
+		return ''.join(random.choices(string.ascii_letters + string.digits, k=self._HASH_LEN))
+
+	def on_connect(self):
+		new_hash = self._generate_user_hash()
+
+		while os.path.isdir('streams/' + new_hash):
+			new_hash = self._generate_user_hash()
+
+			os.makedirs('streams/' + new_hash)
+			self.user_hash = new_hash
+			self.user_dir  = 'streams/' + new_hash
+
+			print("Connected to client. User hash: " + self.user_hash)
+
+	def on_disconnect(self):
+		self.streamer.stop()
+
+		if os.path.isdir(self.user_dir):
+			shutil.rmtree(self.user_dir)
+
+		print("Disconnected from client. User hash: " + self.user_hash)
+		self.user_hash = None
+		self.user_dir  = None
+
+	def _generate_playlist(self):
+		playlist_path = self.user_dir + '/playlist.m3u8'
+
+		print("Generating file: " + playlist_path)
+
+	def on_stream(self, data):
+		if self.streamer == None:
+			self._initialise_streamer(data['url'])
+			self._generate_playlist()
+
+		playlist_path = self.user_dir + '/playlist.m3u8'
+
+		with open(playlist_path, "w") as f:
+			f.write("#EXTM3U\n#EXT-X-VERSION:3\n#EXT-X-TARGETDURATION:9\n#EXT-X-MEDIA-SEQUENCE:0\n")
+
+		print(SERVER_URL + playlist_path)
+
+		emit('stream-response', json.dumps({'media':str(SERVER_URL + playlist_path)}))
+
+socketio.on_namespace(StreamingSocket('/streams'))
 
 if __name__ == '__main__':
-    socketio.run(app)
+	socketio.run(app)
--- server/stream.py
+++ server/stream.py
@@ -11,15 +11,25 @@ from six.moves import queue
 from threading import Thread
 from enum import Enum
 
-BYTES_TO_READ = 500000
+'''
+	New Workflow:
+
+	- On socket connection, create a hash for the user
+	- Streamer starts. It creates a new directory for the user.
+	- It creates a playlist file in this directory and sets off a stream worker.
+	- Every 10 seconds, the stream worker reads from the stream and writes to a temp file.
+	- The stream worker adds the name of the temp file into the current playlist file.
+	- The streamer should return the name of the playlist file to be used by hls.
+
+'''
+
+BYTES_TO_READ = 1000000
 WAIT_TIME	  = 10.0
 
 STD_VIDEO_KEY   = '480p'
-INPUT_FILE 		= "streams/temp"
+INPUT_FILE		= "segment"
 EXTENSION 		= ".ts"
 OUTPUT_WAV_FILE = "temp/audio.wav"
-
-
 
 # Nukes the temp directory and generates a fresh one
 def _clearTempFiles():
@@ -28,12 +38,32 @@ def _clearTempFiles():
 	os.makedirs('temp')
 
 class _StreamWorker(Thread):
-	def __init__(self, buff, stream_data):
+	def __init__(self, buff, stream_data, user_dir):
 		self.buff = buff
 		self.stream_data = stream_data
+		self.user_dir = user_dir
 		self.streaming = True
 		self.count = 0
 		Thread.__init__(self)
+
+	def _update_playlist(self, video_file):
+		playlist_path = self.user_dir + '/playlist.m3u8'
+
+		if not os.isfile(playlist_path):
+			print("Playlist not found, exiting...")
+			raise Exception
+
+		with open(playlist_path, "r") as f:
+			lines = f.readlines()
+
+		sequence_no = int(lines[3].split(':')[1])
+
+		lines[3] = '#EXT-X-MEDIA-SEQUENCE:' + str(sequence_no + 1) + '\n'
+
+		with open(playlist_path, "w+") as f:
+			f.writelines(lines)
+			f.write('#EXTINF:8.0000,\n')
+			f.write(video_file + '\n')
 
 	def run(self):
 		while self.streaming:
@@ -42,68 +72,76 @@ class _StreamWorker(Thread):
 			data = self.stream_data.read(BYTES_TO_READ)
 
 			video_file = INPUT_FILE + str(self.count) + EXTENSION
+			path = self.user_dir + '/'
 
-			with open(video_file, "wb") as f:
+			with open(path + video_file, "wb") as f:
 				f.write(data)
 
 			print("Created file: " + video_file)
+
+			try:
+				self._update_playlist(video_file)
+			except Exception:
+				return
+
 			self.count += 1
 
 		self.stream_data.close()
 
-	def stop_queue_worker(self):
+	def stop(self):
 		self.streaming = False
 
 
 class VideoStreamer(object):
-	def __init__(self, stream_url):
+	def __init__(self, stream_url, user_dir):
 		self.stream_url = stream_url
+		self.user_dir = user_dir
 		self.buffer = queue.Queue()
 		self.sample_rate = None
 		self.worker = None
 
-	def get_data(self, num_segments=5):
-		video_data = self.buffer.get()
+	# def get_data(self, num_segments=5):
+	# 	video_data = self.buffer.get()
 
-		for i in range(1, num_segments):
-			video_data += self.buffer.get()
+	# 	for i in range(1, num_segments):
+	# 		video_data += self.buffer.get()
 
-		video_file = INPUT_FILE + str(self.count) + EXT
-		self.count += 1
+	# 	video_file = INPUT_FILE + str(self.count) + EXT
+	# 	self.count += 1
 
-		with open(video_file, "ab") as f:
-			f.write(video_data)
+	# 	with open(video_file, "ab") as f:
+	# 		f.write(video_data)
 
-		try:
-			audio_data = self._extract_audio(video_file)
-		except Exception as e:
-			raise Exception("FFMpeg Error")
+	# 	try:
+	# 		audio_data = self._extract_audio(video_file)
+	# 	except Exception as e:
+	# 		raise Exception("FFMpeg Error")
 
-		self._set_sample_rate()
+	# 	self._set_sample_rate()
 
-		return (bytearray(video_data), io.BytesIO(audio_data))
+	# 	return (bytearray(video_data), io.BytesIO(audio_data))
 
-	def get_sample_rate(self):
-		return self.sample_rate
+	# def get_sample_rate(self):
+	# 	return self.sample_rate
 
-	def _extract_audio(self, file_name):
-		ff = FFmpeg(
-			inputs={file_name:['-hide_banner', '-loglevel', 'panic', '-y']},
-			outputs={OUTPUT_WAV_FILE:['-ac', '1', '-vn', '-f', 'wav']}
-		)
+	# def _extract_audio(self, file_name):
+	# 	ff = FFmpeg(
+	# 		inputs={file_name:['-hide_banner', '-loglevel', 'panic', '-y']},
+	# 		outputs={OUTPUT_WAV_FILE:['-ac', '1', '-vn', '-f', 'wav']}
+	# 	)
 
-		ff.run()
+	# 	ff.run()
 
-		with open(OUTPUT_WAV_FILE, "rb") as f:
-			content = f.read()
+	# 	with open(OUTPUT_WAV_FILE, "rb") as f:
+	# 		content = f.read()
 
-		return content
+	# 	return content
 
-	def _set_sample_rate(self):
-		if not self.sample_rate:
-			wav = wave.open(OUTPUT_WAV_FILE, "rb")
-			self.sample_rate = wav.getframerate()
-			wav.close()
+	# def _set_sample_rate(self):
+	# 	if not self.sample_rate:
+	# 		wav = wave.open(OUTPUT_WAV_FILE, "rb")
+	# 		self.sample_rate = wav.getframerate()
+	# 		wav.close()
 
 	def _get_video_stream(self):
 		try:
@@ -121,9 +159,6 @@ class VideoStreamer(object):
 
 	def start(self):
 		print("Starting Video Streamer...")
-		print("Clearing Temporary Files...", end="")
-		_clearTempFiles()
-		print("Success!")
 
 		print("Getting Video Stream...", end="")
 		stream = self._get_video_stream()
@@ -132,10 +167,12 @@ class VideoStreamer(object):
 			raise Exception("Streamlink Unavailable")
 		print("Success!")
 
+		print("Opening stream...", end="")
 		data = stream.open()
+		print("Success!")
 
 		print("Starting stream worker...", end="")
-		self.worker = _StreamWorker(self.buffer, data)
+		self.worker = _StreamWorker(self.buffer, data, self.user_dir)
 		self.worker.start()
 		print("Success!")
 
