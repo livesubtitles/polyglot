d4e91b9fad1baed021637d7b693f2b24230130b6
eleni <elenialevra98@gmail.com>
2018-11-10 17:04:36 +0000

    Get page url and language from extension and create iframe for polyglot react

--- webextension/src/embed.js
+++ webextension/src/embed.js
@@ -2,4 +2,19 @@ embedButton.onclick = function(elem) {
        document.getElementById('embedButton').style.visibility = 'hidden';
        document.getElementById('embedButton').style.display = 'none';
        document.getElementById('embedText').style.visibility = 'visible';
+       let languageSelector = document.getElementById('languageSelector');
+       let language = languageSelector.value;
+       chrome.storage.sync.set({'language': language}, function() {
+         console.log("Language set");
+       })
+       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+         let currentTab = tabs[0];
+         let pageUrl = currentTab.url;
+         console.log(pageUrl);
+         let lang = "not_set";
+         chrome.storage.sync.get(['language'], function(result) {
+           lang = result.language;
+         })
+         console.log("Language is " + lang);
+       });
 };
