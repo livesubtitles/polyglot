1840442052ece9e16add9c9e35aa8be81518db25
eleni <elenialevra98@gmail.com>
2018-11-22 15:41:14 +0000

    Fix indentation error

--- server/translate.py
+++ server/translate.py
@@ -33,7 +33,7 @@ def translate(textToTranslate, targetLang, sourceLang, credentials):
 	#     print("Exception with key: {}".format( exc ))
 	#     res = ""
 	# return res
-    json_response = json.loads(content.decode('utf-8'))
+	json_response = json.loads(content.decode('utf-8'))
 	return json_response['data']['translations'][0]['translatedText']
 
 def getLanguages():
