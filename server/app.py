f4cb441b787142484a30ef6bbe11efc2ceecff71
g-ongetta <georgeongetta@gmail.com>
2018-11-23 14:50:08 +0000

    Addded no cache headers to flask responses

--- server/app.py
+++ server/app.py
@@ -122,6 +122,10 @@ def after_request(response):
   response.headers.add('Access-Control-Allow-Origin', '*')
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  response.headers.add('Cache-Control', 'no-cache, no-store, must-revalidate')
+  response.headers.add('Pragma', 'no-cache')
+  response.headers.add('Expires', '0')
+  response.headers.add('Cache-Control', 'public, max-age=0')
   return response
 
 @app.route("/streams")
