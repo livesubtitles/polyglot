29338979a2d271754f80ceb1a0186659bc5b165c
pablo <pablo.gobe@gmail.com>
2018-12-20 23:23:06 +0100

    Stub for adapter

---
+++ server/adapter.py
@@ -0,0 +1,12 @@
+class LanguageDetectionService(object):
+    pass
+    # TODO: fill in with methods
+class TranscriptionService(object):
+    pass
+    #TODO: fill in with methods
+class TranslationService(object):
+    pass
+    #TODO: fill in with methods
+class PunctuatorService(object):
+    pass
+    #TODO: fill in with methods
---
+++ server/tests/test_adapter.py
@@ -0,0 +1,15 @@
+import unittest
+
+class AdapterTest(unittest.TestCase):
+
+    def test_translation_google(self):
+        pass
+
+    def test_transcription_google(self):
+        pass
+
+    def test_punctuating_theano(self):
+        pass
+
+    def test_language_detection_microsoft(self):
+        pass
