924fc3babbe5e1c80c41e9030f98e99395b46545
g-ongetta <georgeongetta@gmail.com>
2019-01-10 16:01:33 +0000

    added str cast

--- server/stream.py
+++ server/stream.py
@@ -222,7 +222,7 @@ class VideoStreamer(object):
         except Exception:
             raise Exception("Streamlink Unavailable")
 
-        print("Available Streams: " + self.available_streams)
+        print("Available Streams: " + str(self.available_streams))
 
         if self.quality not in self.available_streams:
             print("Could not find " + self.quality + " stream")
